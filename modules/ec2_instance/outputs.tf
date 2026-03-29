840ef02a9b3190ae3427e335650d342750607482
Pratik Pandey <pratikpandey0532@gmail.com>
2026-03-29 14:42:26 +0530

    Updated

--- modules/ec2_instance/outputs.tf
+++ modules/ec2_instance/outputs.tf
@@ -1,6 +1,6 @@
 output "ec2_public_ip" {
   value = aws_instance.my_instance.public_ip
 }
-output "ec2_id" {
+output "ec2_id_test" {
   value = aws_instance.my_instance.id
 }
--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "ec2_public_ip" {
   value = module.ec2-dev.ec2_public_ip
 }
-output "ec2_id" {
-  value = module.ec2-dev.ec2_id
+output "ec2_id_test" {
+  value = module.ec2-dev.ec2_id_test
 }
